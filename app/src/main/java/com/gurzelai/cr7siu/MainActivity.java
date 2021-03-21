e083cf22bffbe4064bea3f7ee5d5a358608909db
gurzelai <gorkaurzelai2000@gmail.com>
2021-03-22 00:49:05 +0100

    mi primer commit

---
+++ app/src/main/java/com/gurzelai/cr7siu/MainActivity.java
@@ -0,0 +1,74 @@
+package com.gurzelai.cr7siu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    int numero;
+    ImageView imagen;
+    MediaPlayer mp;
+    SwitchCompat sw;
+    boolean loop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        numero = 1;
+        imagen = findViewById(R.id.imagen);
+        FloatingActionButton boton = findViewById(R.id.boton);
+        boton.setOnClickListener(view -> recargar());
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.cr7);
+        sw = findViewById(R.id.loop);
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loop = !loop;
+                mp.setLooping(loop);
+                if(loop && !mp.isPlaying()){
+                    mp.start();
+                }
+            }
+        });
+        recargar();
+    }
+
+    private void recargar() {
+        if (!loop) {
+            if (mp.isPlaying()) {
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.cr7);
+            }
+        }
+        imagen.setImageResource(getResources().getIdentifier("cr7" + numero++, "drawable", getPackageName()));
+        mp.start();
+        if (numero == 10) numero = 1;
+    }
+
+    private void pantallaCompleta() {
+        getSupportActionBar().hide();
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        pantallaCompleta();
+        recargar();
+    }
+}
